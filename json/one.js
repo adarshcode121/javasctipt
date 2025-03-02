38eba86d963d41758ad4b5680cdf5005a81a022e
Adarsh Prajapati <code.adarsh121@gmail.com>
2025-03-02 18:31:22 +0530

    new json file

---
+++ json/one.js
@@ -0,0 +1,12 @@
+// import data from './first.json'
+
+const data = require('./first.json');
+// console.log(data);
+
+
+// ad = JSON.parse(data)
+// console.log(ad);
+
+console.log(data);
+
+console.log(data.myObj.Items[0]);
---
+++ loops/l1.js
@@ -0,0 +1,6 @@
+for (let ad = 0; ad <=5; ad++) {
+    const num = ad;
+    console.log(ad);
+
+    
+}
