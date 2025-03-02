52ab0a3ae43b71fa08218740bc56f3ceb1b6fe11
Adarsh Prajapati <code.adarsh121@gmail.com>
2025-03-02 22:39:57 +0530

    New File add 44# Please enter the commit message for your changes. Lines starting

---
+++ loops/while.js
@@ -0,0 +1,45 @@
+// let num=0
+// while (num<10) {
+//     console.log(`num:${num}`);
+//     num++
+// }
+
+// let myArray=['flash','Batman','supermain',"Saktiman"];
+
+// let arr=0;
+// while (arr<=myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+    
+// }
+
+
+
+// let score =11
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score <10);
+
+
+
+// for of 
+
+
+let arr =['Adarsh','Akash','Vikash','Shivam','laxmi']
+for (const elm of arr) {
+    // console.log(`Elements of Array  is : ${elm}`);
+    
+}
+
+
+let name = "Adarsh Prajapti"
+
+
+for (const nm of name) {
+    console.log(nm);
+    
+}
+
+
