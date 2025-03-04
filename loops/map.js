873ee9d61067f3cf638d2102a9c84da01230c724
Adarsh Prajapati <code.adarsh121@gmail.com>
2025-03-04 16:54:11 +0530

    new commit

---
+++ loops/map.js
@@ -0,0 +1,26 @@
+const myObj={
+    name : "Adash",
+    roll_no:100,
+    city:"Silvassa"
+}
+
+const myObj1={
+   " name ": "Ankit",
+   " roll_no":100,
+    "city":"Silvassa"
+}
+
+console.log(myObj.name);
+console.log(myObj1[" name "]);
+
+for (key  in  myObj1) {
+    console.log(`The key is : ${key} and the value is : ${value}`);
+        
+    }
+
+
+
+
+
+
+    // Jay shree ram 
