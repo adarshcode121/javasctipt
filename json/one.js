7f7a062eb4a048dc98b29c64f3e2afce0c832dc8
Adarsh Prajapati <code.adarsh121@gmail.com>
2025-03-04 21:45:42 +0530

    added new lines in one.js

--- json/one.js
+++ json/one.js
@@ -9,4 +9,6 @@ const data = require('./first.json');
 
 console.log(data);
 
-console.log(data.myObj.Items[0]);
+// console.log(data.myObj.Items[0]);
+
+console.log(data.myObj.name,data.myObj.Items[2]);
