518b58c1caf6a033806bb85a6668630e3cd7343e
Adarsh Prajapati <code.adarsh121@gmail.com>
2025-03-04 23:18:38 +0530

    some changes

--- loops/map.js
+++ loops/map.js
@@ -1,26 +1,49 @@
-const myObj={
-    name : "Adash",
-    roll_no:100,
-    city:"Silvassa"
-}
+// const myObj={
+//     name : "Adash",
+//     roll_no:100,
+//     city:"Silvassa"
+// }
 
-const myObj1={
-   " name ": "Ankit",
-   " roll_no":100,
-    "city":"Silvassa"
-}
+// const myObj1={
+//    " name ": "Ankit",
+//    " roll_no":100,
+//     "city":"Silvassa"
+// }
 
-console.log(myObj.name);
-console.log(myObj1[" name "]);
+// console.log(myObj.name);
+// console.log(myObj1[" name "]);
 
-for (key  in  myObj1) {
-    console.log(`The key is : ${key} and the value is : ${value}`);
+// for (key  in  myObj1) {
+//     console.log(`The key is : ${key} and the value is : ${value}`);
         
-    }
+//     }
+
+
+//only adds unique values 
+
+const map = new Map();
+
+map.set('IN',"India")
+map.set('USA',"United States Of America")
+map.set('Fr',"France")
+map.set('IN',"India")
+
+
+for (const [key,value] of map) {
+    // console.log(key ," :-" ,value);
+    
+}
+
+const myObj ={
+    'game1':'pubg',
+    'game2':'free fire'
+}
+
+for (const key in myObj) {
+   
+    console.log(`${key} is ${myObj[key]} `);
+}
 
 
 
 
-
-
-    // Jay shree ram 
